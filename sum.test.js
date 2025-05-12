269c8ed0a29ba1032f047eeb76b5a98fc31ddf19
Sergio Marques <sergio.marques@puc-campinas.edu.br>
2025-05-12 10:47:40 -0300

    novo teste

--- sum.test.js
+++ sum.test.js
@@ -6,3 +6,10 @@ test('Soma 1 + 2 igual a 3', () => {
    expect(sum(1,2)).toBe(3);
 });
 
+describe("teste 2 ", () => {
+
+	test(' soma de 2 + 2 deve ser 4 ', () => {
+		expect(sum(2,2)).toBe(4);
+	});
+
+}); 
